9d4593bb20b9eaa3aa4ff2bddaa889ea83743526
12345shivaleela <96932959+12345shivaleela@users.noreply.github.com>
2023-05-17 13:41:45 +0530

    Add files via upload

---
+++ stream1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+def main():
+ st.title("Shivaleela Streamlit app")
+ st.sidebar.title("Sidebar")
+ # Add content to the app
+ st.write("Welcome to Shivaleela app!")
+if __name__ == "__main__":
+   main()
+
+
+
+
